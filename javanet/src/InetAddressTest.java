a10db9e8015cd2fa33e2f79d58d8a87b779acb52
licheng <291215654@qq.com>
2018-01-08 21:34:17 +0800

    添加算法基础代码

--- javanet/src/InetAddressTest.java
+++ javanet/src/InetAddressTest.java
@@ -1,13 +1,50 @@
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by licheng on 1/4/18.
  */
 public class InetAddressTest {
     public static void main(String[] args) throws IOException {
-        InetAddress ip = InetAddress.getByName("localhost");
-        boolean r = ip.isReachable(5000);
-        System.out.println(r);
+//        InetAddress ip = InetAddress.getByName("localhost");
+//        boolean r = ip.isReachable(5000);
+//        System.out.println(r);
+
+
+        char[] a = {'a','f','d','a','f','a','s','d','d'};
+        char b = '\141';
+        System.out.println(b);
+
+
+
+
+    }
+}
+
+
+class Cat extends Thread{
+    public static void sleep(){
+
+        System.out.println("My sleep!");
+
+    }
+}
+
+enum AccountType
+{
+    SAVING, FIXED, CURRENT;
+    private AccountType()
+    {
+        System.out.println("It is a account type");
+    }
+}
+class EnumOne
+{
+    public static void main(String[]args)
+    {
+        System.out.println(AccountType.FIXED);
     }
 }
