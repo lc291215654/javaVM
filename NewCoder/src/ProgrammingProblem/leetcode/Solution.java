4ec81269fadf40005ebb35bbf359da1a27f11025
licheng <291215654@qq.com>
2018-08-26 16:49:52 +0800

    add

--- NewCoder/src/ProgrammingProblem/leetcode/Solution.java
+++ NewCoder/src/ProgrammingProblem/leetcode/Solution.java
@@ -1,7 +1,9 @@
 package ProgrammingProblem.leetcode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by licheng on 8/25/18.
@@ -10,11 +12,20 @@ public class Solution {
 
     public static void main(String args[]) {
 
+        char[][] board = new char[][]{{'o', 'a', 'a', 'n'},
+                {'e', 't', 'a', 'e'},
+                {'i', 'h', 'k', 'r'},
+                {'i', 'f', 'l', 'v'}};
+        Solution solution = new Solution();
+        System.out.println(solution.findWords(board,new String[]{"eathflv","tae","fdaf"}));
+
     }
-    class TrieNode{
+
+    class TrieNode {
         int path;
         int end;
         TrieNode[] map;
+
         public TrieNode() {
             path = 0;
             end = 0;
@@ -22,11 +33,13 @@ public class Solution {
         }
 
     }
+
     class Trie {
         /**
          * Initialize your data structure here.
          */
         TrieNode root;
+
         public Trie() {
             root = new TrieNode();
         }
@@ -35,15 +48,15 @@ public class Solution {
          * Inserts a word into the trie.
          */
         public void insert(String word) {
-            if(word == null){
+            if (word == null) {
                 return;
             }
             char[] chs = word.toCharArray();
             TrieNode p = root;
             int index = 0;
-            for(int i=0;i<chs.length;i++){
+            for (int i = 0; i < chs.length; i++) {
                 index = chs[i] - 'a';
-                if(p.map[index] == null){
+                if (p.map[index] == null) {
                     p.map[index] = new TrieNode();
                 }
                 p = p.map[index];
@@ -69,20 +82,56 @@ public class Solution {
         }
     }
 
-
     public List<String> findWords(char[][] board, String[] words) {
-
-        List<String> result = new ArrayList<>();
+        Set<String> result = new HashSet<>();
         for(String word:words){
-            if (findword(word)){
+            if(isExsts(board,word)){
                 result.add(word);
             }
         }
-        return null;
+        return new ArrayList<>(result);
     }
 
-    private boolean findword(String word) {
+    public boolean isExsts(char[][] board, String words) {
 
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, visited, i, j, words, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    List<String> result = new ArrayList<>();
+
+    public boolean dfs(char[][] board, boolean[][] visit, int i, int j, String words, int k) {
+        System.out.println("i:" + i +" j:" + j + " K:" + k);
+        if (k == words.length()) {
+            return true;
+        }
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+            return false;
+        }
+        if (visit[i][j] == true) {
+            return false;
+        }
+        if (board[i][j] != words.charAt(k)) {
+            return false;
+        }
+        visit[i][j] = true;
+        if (dfs(board, visit, i - 1, j, words, k+1) ||
+                dfs(board, visit, i + 1, j, words, k+1) ||
+                dfs(board, visit, i, j - 1, words, k+1) ||
+                dfs(board, visit, i, j + 1, words, k+1)) {
+            return true;
+        }
+        visit[i][j] = false;
         return false;
     }
 
