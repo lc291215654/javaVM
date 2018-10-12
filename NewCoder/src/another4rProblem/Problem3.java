d7ba7cc1a7e737ccf56822e05c89b89a8a5deb4c
licheng <291215654@qq.com>
2018-10-12 16:42:21 +0800

    leet

---
+++ NewCoder/src/another4rProblem/Problem3.java
@@ -0,0 +1,43 @@
+package another4rProblem;
+
+/**
+ * Created by licheng on 10/12/18.
+ */
+public class Problem3 {
+
+
+
+    static class Solution {
+
+        /**
+         * 191. Number of 1 Bits
+         * @param n
+         * @return
+         */
+        public int hammingWeight(int n) {
+            int sum =0;
+            while(n != 0){
+                sum++;
+                n = n & (n-1);
+            }
+            return sum;
+        }
+
+        /**
+         * 461. Hamming Distance
+         * @param x
+         * @param y
+         * @return
+         */
+        public int hammingDistance(int x, int y) {
+            int n = x ^ y;
+            int sum =0;
+            while(n != 0){
+                sum++;
+                n = n & (n-1);
+            }
+            return sum;
+
+        }
+    }
+}
